3a1fb562c64630d35c7efa48d5d989eddfc93268
HelloGaoo <gao13053915898@qq.com>
2026-03-21 15:49:33 +0800

    fix: 修正URL路径

--- version.py
+++ version.py
@@ -3,6 +3,6 @@ BUILD_DATE = "2026-03-14"
 
 __version__ = VERSION
 
-VERSION_URL = "https://ghfile.geekertao.top/https://github.com/HelloGaoo/ClassLively/main/version.py"
-UPDATE_URL = "https://ghfile.geekertao.top/https://github.com/HelloGaoo/ClassLively/main/update.7z"
-CHANGELOG_URL = "https://ghfile.geekertao.top/https://github.com/HelloGaoo/ClassLively/main/changelog.md"
+VERSION_URL = "https://ghfile.geekertao.top/https://github.com/HelloGaoo/ClassLively/blob/main/version.py"
+UPDATE_URL = "https://ghfile.geekertao.top/https://github.com/HelloGaoo/ClassLively/blob/main/update.7z"
+CHANGELOG_URL = "https://ghfile.geekertao.top/https://github.com/HelloGaoo/ClassLively/blob/main/changelog.md"
