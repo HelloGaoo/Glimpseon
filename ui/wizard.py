db4194b18a6ad00b2374a21418c9ead5af99e712
HelloGaoo <gao13053915898@qq.com>
2026-04-12 13:51:57 +0800

    feat(ui): 添加应用图标和欢迎页面图标显示

--- ui/wizard.py
+++ ui/wizard.py
@@ -3,22 +3,27 @@ import json
 import os
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QVBoxLayout, QDialog, QStackedWidget, QWidget
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QVBoxLayout, QDialog, QStackedWidget, QWidget, QLabel
 from qfluentwidgets import (
     BodyLabel,
     MessageBox,
     PrimaryPushButton,
 )
 
-from core.constants import BASE_DIR
+from core.constants import BASE_DIR, get_resPath
 
 
 class WizardWindow(QDialog):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("ClassLively 向导")
-        self.setFixedSize(600, 400)
+        self.setFixedSize(750, 550)
         self.setWindowFlags(Qt.Window | Qt.WindowCloseButtonHint)
+
+        icon_path = get_resPath(os.path.join("resource", "icons", "CY.png"))
+        if os.path.exists(icon_path):
+            self.setWindowIcon(QIcon(icon_path))
 
         self.mainLayout = QVBoxLayout(self)
         self.mainLayout.setContentsMargins(40, 40, 40, 40)
@@ -30,7 +35,14 @@ class WizardWindow(QDialog):
         self.page1 = QWidget()
         self.page1Layout = QVBoxLayout(self.page1)
         self.page1Layout.setAlignment(Qt.AlignCenter)
-        self.page1Layout.setSpacing(30)
+        self.page1Layout.setSpacing(20)
+
+        self.iconLabel = QLabel(self.page1)
+        self.iconLabel.setFixedSize(128, 128)
+        self.iconLabel.setAlignment(Qt.AlignCenter)
+        if os.path.exists(icon_path):
+            pixmap = QPixmap(icon_path)
+            self.iconLabel.setPixmap(pixmap.scaled(128, 128, Qt.KeepAspectRatio, Qt.SmoothTransformation))
 
         self.welcomeLabel = BodyLabel("ClassLively", self.page1)
         self.welcomeLabel.setStyleSheet("font-size: 48px; font-weight: bold;")
@@ -39,7 +51,8 @@ class WizardWindow(QDialog):
         self.nextButton = PrimaryPushButton("下一步", self.page1)
         self.nextButton.setFixedSize(160, 40)
 
-        self.page1Layout.addWidget(self.welcomeLabel)
+        self.page1Layout.addWidget(self.iconLabel, 0, Qt.AlignCenter)
+        self.page1Layout.addWidget(self.welcomeLabel, 0, Qt.AlignCenter)
         self.page1Layout.addWidget(self.nextButton, 0, Qt.AlignCenter)
 
         self.stackedWidget.addWidget(self.page1)
